031278ded2a0e33e4de391f32a8d76171c517392
mariua-galashina-kononkova-qa <166447839+mariua-galashina-kononkova-qa@users.noreply.github.com>
2024-05-12 21:37:02 +0300

    Add files via upload

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,92 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный логин и верный пароль', function () {
+         cy.visit('https://login.qa.studio/'); // Зашли на сайт
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+         
+         cy.get('#mail').type('german@dolnikov.ru'); // Ввели верный логин
+         cy.get('#pass').type('iLoveqastudio1'); // Ввели верный пароль
+         cy.get('#loginButton').click(); // Нажала войти
+         
+         cy.get('#messageHeader').contains('Авторизация прошла успешно'); // Проверяю, что после авторизации вижу текст
+         cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+     })
+  
+
+     it('Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+        
+        cy.get('#mail').type('german@dolnikov.ru'); // Ввели верный логин
+        cy.get('#pass').type('iLoveqastudio23'); // Ввели неверный пароль
+        cy.get('#loginButton').click(); // Нажала войти
+        
+        cy.get('#messageHeader').contains('Такого логина или пароля нет'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+ 
+    it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+        
+        cy.get('#mail').type('mv@galashina.ru'); // Ввели неверный логин
+        cy.get('#pass').type('iLoveqastudio1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажала войти
+        
+        cy.get('#messageHeader').contains('Такого логина или пароля нет'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+
+    it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+        
+        cy.get('#forgotEmailButton').click(); // Нажала на кнопку "Забыли пароль"
+        cy.get('#mailForgot').type('german@dolnikov.ru'); // Ввели имейл для восстановления
+        cy.get('#restoreEmailButton').click(); // Нажала "Отправить код""
+        
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+
+    it('Проверка, что в логине есть @', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+        
+        cy.get('#mail').type('germandolnikov.ru'); // Ввели логин без @
+        cy.get('#pass').type('iLoveqastudio1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажала войти
+        
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+    it('Проверку на приведение к строчным буквам в логине', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+        
+        cy.get('#mail').type('GerMan@Dolnikov.ru'); // Ввели логин с заглавными буквами
+        cy.get('#pass').type('iLoveqastudio1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажала войти
+        
+        cy.get('#messageHeader').contains('Такого логина или пароля нет'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+})
+
+
+
+ 
+
+ 
+  
+
